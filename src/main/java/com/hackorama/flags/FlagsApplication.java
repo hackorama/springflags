384194166bd3e33c7d2e12f01472cf738ec7634c
hackorama <kishan.thomas@gmail.com>
2019-02-05 16:04:42 -0800

    Enabling key value data

--- src/main/java/com/hackorama/flags/FlagsApplication.java
+++ src/main/java/com/hackorama/flags/FlagsApplication.java
@@ -2,8 +2,16 @@ package com.hackorama.flags;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.map.repository.config.EnableMapRepositories;
 
+/**
+ * Sprint boot application entry point
+ *
+ * @author Kishan Thomas (kishan.thomas@gmail.com)
+ *
+ */
 @SpringBootApplication
+@EnableMapRepositories
 public class FlagsApplication {
 
 	public static void main(String[] args) {
